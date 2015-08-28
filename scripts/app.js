d26419c1ff359b4ee9ecf6cde5e58ef4d9bf0553
Paul Kim <paulkim517@gmail.com>
2015-08-28 10:33:25 -0700

    refactored to use angular

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,20 @@
-angular.module('starter',[]
+angular.module('starter',[])
 .controller("MainCtrl", ['$scope', '$rootScope', function ($scope, $rootScope) {
   $scope.term = "cellar door"
   $scope.showAlert = function() {
     alert($scope.term);
   }	
-});
+}])
+
 .controller("PicCtrl", ['$scope', function ($scope) {
-  beforeClick = null 
+  
+  $scope.photo = "../shia/images/shia.jpg"
+  $scope.ask = function(){
+  	$scope.photo = "http://media.giphy.com/media/87xihBthJ1DkA/giphy.gif"
+  }
+
+}]);
 
 
-});
 
+// http://media.giphy.com/media/87xihBthJ1DkA/giphy.gif
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,21 +16,7 @@ $('#start').on('click', function(e){
 
 
     
-        
-// <audio id="nooo">
-//         <source src="nooo.ogg" type="audio/ogg">
-//         <source src="nooo.mp3" type="audio/mpeg">
-//         <source src="nooo.mp4" type="audio/mp4">
-//         <script language="javascript">
-//          
-//         
-//           &lt;object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" width="550" height="400" id="button" align="middle"&gt;
-//           &lt;param name="allowScriptAccess" value="sameDomain" /&gt;
-//           &lt;param name="allowFullScreen" value="false" /&gt;
-//           &lt;param name="movie" value="button.swf" /&gt;&lt;param name="quality" value="high" /&gt;&lt;param name="bgcolor" value="#ffffff" /&gt;  &lt;embed src="button.swf" quality="high" bgcolor="#ffffff" width="550" height="400" name="button" align="middle" allowScriptAccess="sameDomain" allowFullScreen="false" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" /&gt;
-//           &lt;/object&gt;
-//         </noscript>
-//       </audio>
+
 
 
 
